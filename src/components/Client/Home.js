97936848d1424c53f3f3e489d642e8452408fec2
iSaacSigei <kiplangatisack.is@gmail.com>
2023-04-17 09:11:36 +0300

    minor changes

--- src/components/Client/Home.js
+++ src/components/Client/Home.js
@@ -19,13 +19,13 @@ const Home = ({user}) => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               {user.error==="Not authorized"?(<Link
-                to="/signup"
+                to="/user/landform"
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
                 Get started
               </Link>):
               (<Link
-                to="/user/landform"
+                to="/signup"
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
                 Get started
